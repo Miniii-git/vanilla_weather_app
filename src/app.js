694998e9867d7904d90e53eb7177c04a92e93f59
Miniii-git <125822323+Miniii-git@users.noreply.github.com>
2023-02-27 01:27:49 +0330

    write js code and use api for search city's (day hour min )

--- src/app.js
+++ src/app.js
@@ -27,10 +27,34 @@ function getInformation(response){
 
     let description = document.querySelector("#wea-description");
     description.innerHTML = response.data.condition.description;
-    
+
     let image = document.querySelector("img")
     image.setAttribute("alt",response.data.condition.icon);
     image.setAttribute("src",response.data.condition.icon_url);
+
+    let timestamp = response.data.time;
+    let date = new Date(timestamp*1000);
+    let day = document.querySelector("#day");
+    let hour = document.querySelector("#hour");
+    let min = document.querySelector("#min");
+    let d = date.getDay();
+    let h = date.getHours();
+    let m = date.getMinutes();
+
+    let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+    day.innerHTML = days[d]
+
+    if(h<=9){
+        hour.innerHTML = `0${h}`;
+    }else{
+        hour.innerHTML =h;
+    }
+     
+    if(m<=9){
+        min.innerHTML = `0${m}`;
+    }else{
+        min.innerHTML =m;
+    }
 }
 
 let searchForm = document.querySelector("#search-form");
